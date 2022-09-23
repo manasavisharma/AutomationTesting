399bd65c6a6b66378b9260518ef0580babcc20d3
Manasavi Sharma <sharma.manasavi@gmail.com>
2022-09-23 13:28:07 +0530

    JsAlert File

--- src/test/java/basicAuth.java
+++ src/test/java/basicAuth.java
@@ -1,15 +1,35 @@
-import org.openqa.selenium.WebDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class basicAuth {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        //=====Working on Booking(For Alerts).
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\pc\\Downloads\\chromedriver_win32\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://the-internet.herokuapp.com/basic_auth");
+        driver.get("https://the-internet.herokuapp.com/");
 
+        driver.findElement(By.xpath("//a[contains(text(),'Basic Auth')]")).click();
+
+        Alert alt = driver.switchTo().alert();
+        String altmsg = driver.switchTo().alert().getText();
+        Thread.sleep(2000);
+        System.out.println(altmsg);
+        Thread.sleep(2000);
+        alt.accept();
+
+        //press tab
+        //enter password
+        //press tab and click on sign in button
+        //or press tab 2 times to cancel the login
+        
     }
+
 }
